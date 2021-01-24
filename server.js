1ffa8ba40964f5e483d961edb9b2793b70be30b0
Luboho <testujem.php@gmail.com>
2021-01-24 18:26:59 +0100

    server.js edit

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
- // server.js
- var express = require('express');
- var path = require('path');
- var serveStatic = require('serve-static');
- var app = express();
- var history = require('connect-history-api-fallback');
- 
- app.use(history({
-   // verbose: true
- }));
- 
- app.use(express.static(__dirname + '/dist'));
- 
- // var port = process.env.PORT || 8080;
- 
- app.set('port', (process.env.PORT || 8080));
- 
- 
- // app.listen(port);
- 
- app.listen(app.get('port'), () => {
-   console.log(`ENV: ${process.env.NODE_ENV}`);
-   console.log(`Derp is running at localhost: ${app.get('port')}`);
- });
- 
- // console.log('server started '+ port);
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 8080
+app.listen(port)
+
+
