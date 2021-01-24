e392b111bcb26c699c4eb3a3302c7eb86637cd9f
Luboho <testujem.php@gmail.com>
2021-01-24 19:12:31 +0100

    deploy

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+// const express = require('express')
+// const serveStatic = require('serve-static')
+// const path = require('path')
 
-const app = express()
+// const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 8080
+// const port = process.env.PORT || 8080
+// app.listen(port)
+
+var history = require('connect-history-api-fallback');
+
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+app = express()
+//add this middleware
+app.use(history());    
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 8080
 app.listen(port)
-
-
+console.log('server started '+ port) 
