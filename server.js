95fec989d57cb780784025e195e29f8c7e9ee242
Luboho <testujem.php@gmail.com>
2021-01-24 19:31:44 +0100

    deploy

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-// const express = require('express')
-// const serveStatic = require('serve-static')
-// const path = require('path')
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-// const app = express()
+const app = express()
 
-// app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// const port = process.env.PORT || 8080
-// app.listen(port)
+const port = process.env.PORT || 8080
+app.listen(port)
 
 var history = require('connect-history-api-fallback');
 
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express()
-//add this middleware
+// var express = require('express')
+// var path = require('path')
+// var serveStatic = require('serve-static')
+// app = express()
+// //add this middleware
 app.use(history());    
-app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started '+ port) 
+// app.use(serveStatic(__dirname))
+// var port = process.env.PORT || 5000
+// app.listen(port)
+// console.log('server started '+ port) 
+
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
--- src/router/index.js
+++ src/router/index.js
@@ -41,5 +41,4 @@ const router = new VueRouter({
 });
 
 
-
 export default router;
